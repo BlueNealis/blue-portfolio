38127093b6ee09a05c80abda5d151e7d971201a8
Blue Jessen <canealisblue@gmail.com>
2023-01-16 20:41:10 -0800

    Add a transition on hover for each technology

--- components/TechnologyImage.js
+++ components/TechnologyImage.js
@@ -4,7 +4,7 @@ import styles from "./TechnologyImage.module.css"
 
 const TechnologyImage = ({source}) => {
     return(
-    <Image className={styles.icon} src={source} alt='technology icon' width={200} height={75}></Image>
+    <Image className={styles.icon} src={source} alt='technology icon' width={'250'} height={'75'}></Image>
         )
 }
 
