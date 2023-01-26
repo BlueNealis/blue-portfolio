171c284339fa11f7090dd4fdd28e25ac13e01c73
Blue Jessen <canealisblue@gmail.com>
2023-01-25 19:06:54 -0800

    Add vars file and color variables

--- public/projects.js
+++ public/projects.js
@@ -6,19 +6,20 @@ const projects = [{
     deployed:"",
     technologies:['Next.js', "React", "Sass", "Vercel", "Es6", "HTML"], 
     features:["User can put in preferences of alcohol and flavor for customized flavor suggestions",
-    "User can get random drink suggestions", "Responsive styling allows for ease of use on mobile and desktop"]
+    "User can get random drink suggestions", 
+    "Responsive styling allows for ease of use on mobile and desktop"]
 },{
     title: "Birds of A Feather", 
     img:"/../public/project-images/BoAF.png", 
     deployed:"",
     technologies:["GraphQL", "MapQuest API", "React", "Postman", "Cypress", "Circle CI", "Apollo", "Rails", "Es6"], 
     features:["User can create events for other families to go to",
-     "User can rsvp to different events",
+       "User can rsvp to different events",
       "User can view events in their area in both a map view and list view",
-    "Responsive design allows for mobile and desktop use", 
-    "Mapquest API used so user can have visual of where event is in relation to them", 
-    "Postman server used for fetch requests while backend being built out", 
-    "Cypress used for e2e testing to ensure user experience is as expected"]
+      "Responsive design allows for mobile and desktop use", 
+      "Mapquest API used so user can have visual of where event is in relation to them",
+      "Postman server used for fetch requests while backend being built out", 
+      "Cypress used for e2e testing to ensure user experience is as expected"]
 },{
     title: "Discoverd", 
     img:"/../public/project-images/discoverd.png", 
